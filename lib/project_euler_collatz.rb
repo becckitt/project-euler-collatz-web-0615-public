4758b79ca638599d71ae05655acd740495f1548a
Flatiron School <flatironschool@Flatirons-MacBook-Air.local>
2015-08-19 09:25:46 -0400

    progress

--- lib/project_euler_collatz.rb
+++ lib/project_euler_collatz.rb
@@ -1,14 +1,33 @@
 def even_next(n)
+	n/2
 end
 
 def odd_next(n)
+	3*n + 1
 end
 
 def next_value(n)
+	if n % 2 == 0
+		even_next(n)
+	else
+		odd_next(n)
+	end
 end
 
 def collatz(n)
+	sequence = [n]
+	until n == 1
+		next_val = next_value(n)
+		sequence << next_val
+		n = next_val
+	end
+	sequence
 end
 
 def longest_collatz
+	range = {}
+	(1000000..1).each do |num|
+		range[collatz(n).length] = collatz(n)
+	end
+	range #.sort.last
 end
